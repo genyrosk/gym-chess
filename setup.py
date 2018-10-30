9f04ec9ed75732591432ad146a4e7974c868ad3d
Aly Shmahell <aly.shmahell@gmail.com>
2018-10-30 01:29:46 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup
 setup(
 	name='gym_chess',
 	version='0.0.1',
-	install_requires=['gym']
-) 
+	packages=['gym_chess']
+	install_requires=['gym',
+		'pprint',
+		'numpy'
+		],
+	
+)
