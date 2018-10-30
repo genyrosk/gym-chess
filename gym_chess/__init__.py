3382e44c2220144d405352415bad469a3c87fab7
Aly Shmahell <aly.shmahell@gmail.com>
2018-10-30 01:44:08 +0100

    fixed package folder

--- gym_chess/__init__.py
+++ gym_chess/__init__.py
@@ -1,17 +1,14 @@
-from envs import *
-from test import *
+import ChessEnv
 from gym.envs.registration import register
 
 register(
 	id='ChessVsRandomBot-v0',
-	entry_point='gym_chess.envs:ChessEnv',
+	entry_point='gym_chess:ChessEnv',
 	kwargs={'opponent' : 'random'},
 )
 
 register(
 	id='ChessVsSelf-v0',
-	entry_point='gym_chess.envs:ChessEnv',
+	entry_point='gym_chess:ChessEnv',
 	kwargs={'opponent' : 'none'},
-	# max_episode_steps=100,
-	# reward_threshold=.0, # optimum = .0
 )
