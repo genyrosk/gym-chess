fb601d360c54b094bd309cd5631b46109024f3c9
AlyShmahell <aly.shmahell@gmail.com>
2019-02-15 21:32:39 +0100

    made the package installable

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
+import sys
 
+if sys.version_info < (3, 5):
+	raise Exception('Only Python 3.5 and above is supported.')
 setup(
 	name='gym_chess',
 	version='0.0.1',
-	install_requires=['gym']
-) 
+	packages=['gym_chess', 'gym_chess_examples'],
+	install_requires=['gym',
+		'pprint',
+		'numpy'
+		]
+)
